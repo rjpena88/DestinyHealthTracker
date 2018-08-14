42bc3638815f7b6d58fe623f6995d3463ce85de2
Unknown <maleach16@gmail.com>
2018-08-14 10:58:16 -0500

    Small card changes

--- src/pages/cards/cards.ts
+++ src/pages/cards/cards.ts
@@ -12,33 +12,6 @@ export class CardsPage {
   constructor(public navCtrl: NavController) {
     this.cardItems = [
       {
-        user: {
-          avatar: 'assets/img/marty-avatar.png',
-          name: 'Marty McFly'
-        },
-        date: 'November 5, 1955',
-        image: 'assets/img/advance-card-bttf.png',
-        content: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
-      },
-      {
-        user: {
-          avatar: 'assets/img/sarah-avatar.png.jpeg',
-          name: 'Sarah Connor'
-        },
-        date: 'May 12, 1984',
-        image: 'assets/img/advance-card-tmntr.jpg',
-        content: 'I face the unknown future, with a sense of hope. Because if a machine, a Terminator, can learn the value of human life, maybe we can too.'
-      },
-      {
-        user: {
-          avatar: 'assets/img/ian-avatar.png',
-          name: 'Dr. Ian Malcolm'
-        },
-        date: 'June 28, 1990',
-        image: 'assets/img/advance-card-jp.jpg',
-        content: 'Your scientists were so preoccupied with whether or not they could, that they didn\'t stop to think if they should.'
-      },
-      {
         //Prescription Array ( Need rX API! )
         user: {
           avatar: 'assets/img/rX-avatar.png',
@@ -63,5 +36,9 @@ export class CardsPage {
       },
     ];
 
+  }
+
+  getData(){
+    console.log("clicked");
   }
 }
