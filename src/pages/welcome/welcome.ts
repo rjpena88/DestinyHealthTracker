c2a68e65b97fc68bd1ce4ae9960c560878b6d902
Jeremy P <rolandojpena88@gmail.com>
2018-08-20 12:07:52 -0500

    changes needed to compile for QA

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController} from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 import { MainPage } from '../';
 import { DataService } from '../../providers/data/data.service';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { auth } from 'firebase';
-
-
-/**
- * The Welcome Page is a splash page that quickly describes the app,
- * and then directs the user to create an account or log in.
- * If you'd like to immediately put the user onto a login/signup page,
- * we recommend not using the Welcome page.
-*/
+// import { auth } from 'firebase';
 
 @IonicPage()
 @Component({
@@ -20,32 +12,37 @@ import { auth } from 'firebase';
 })
 export class WelcomePage {
 
-  email:string;
-  password:string;
+  email: string;
+  password: string;
 
-// Move to cards.ts
-  constructor(public navCtrl: NavController, private auth: DataService, private fireAuth: AngularFireAuth) {
+  // Move to cards.ts
+  constructor(public navCtrl: NavController, private auth: DataService, private fireAuth: AngularFireAuth, public afAuth: AngularFireAuth, ) {
     this.auth.getDrugById(61).subscribe(console.log);
-   }
+  }
 
   login() {
     this.auth.login(this.email, this.password).then((data) => {
       this.navCtrl.push(MainPage);
-    }, (error:any) => {
+    }, (error: any) => {
       this.navCtrl.push(WelcomePage);
     });
   }
+
+  logout() {
+    return this.afAuth.auth.signOut();
+  }
+
 
   signup() {
     this.navCtrl.push('SignupPage');
   }
 
-  // Google Auth Sign Im
-  signIn() {
-    // Sign in Firebase using popup auth and Google as the identity provider.
-    // var provider = new firebase.auth.GoogleAuthProvider();
-    // firebase.auth().signInWithPopup(provider);
-    this.fireAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  // Ionic Google Auth Sign In - NOT IN USE
+  // signIn() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  // var provider = new firebase.auth.GoogleAuthProvider();
+  // firebase.auth().signInWithPopup(provider);
+  // this.fireAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
 
-  }
 }
+
